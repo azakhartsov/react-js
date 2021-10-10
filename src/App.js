e4b61b0b852d22dc1df679bdff0e60c354a817b4
Andrew <nikamoda@gmail.com>
2021-10-10 20:37:55 +1100

    react u1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+let text = "test variable";
 
 function App() {
+  let text2 = "222";
+  const style = {
+    fontSize: "24px",
+    fontStyle: "italic",
+    color: "red",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 style={style}>app_1</h1>
+        <img src="/images/gato.png" alt="un gato" />
+        <ul>
+          <li>Hello</li>
+          <li>{text + text2}</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
