6d1128fd49f6d12b550ba20433d8bc42cd35c672
Andrew <nikamoda@gmail.com>
2022-01-19 22:36:49 +1100

    u19

--- unit_16/react_project/src/components/Note.js
+++ unit_16/react_project/src/components/Note.js
@@ -1,5 +1,74 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import env from "../env.json";
+
 function Note() {
-  return <div></div>;
+  let { noteURL } = useParams();
+  const [noteText, setNoteText] = useState("");
+  const [lineClass, setLineClass] = useState("hide");
+  const [formClass, setFormClass] = useState("hide");
+  const [errorClass, setErrorClass] = useState("hide");
+
+  useEffect(() => {
+    if (noteURL !== undefined) {
+      fetch(env.urlBackend, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: JSON.stringify({ url: noteURL }),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          if (response.result) {
+            setNoteText(response.note);
+            setLineClass("");
+            setFormClass("hide");
+            setErrorClass("hide");
+          } else if (!response.result) {
+            setLineClass("hide");
+            setFormClass("hide");
+            setErrorClass("");
+          }
+        });
+    } else {
+      setLineClass("hide");
+      setFormClass("");
+      setErrorClass("hide");
+    }
+  }, []);
+
+  function getNote(event) {
+    event.preventDefault();
+    let url = event.target.elements.url.value;
+    url = url.trim();
+    if (url === "") {
+      alert("Fill up fields");
+      return false;
+    }
+    noteURL = url;
+    window.location.href = env.url + "/" + url;
+  }
+
+  return (
+    <div>
+      <div className={lineClass}>
+        <h4>Note:</h4>
+        <div>{noteText}</div>
+      </div>
+      <div className={errorClass}>
+        <p>Error. This hash has not been found!</p>
+      </div>
+      <div className={formClass}>
+        <form action="" onSubmit={getNote}>
+          <label htmlFor="url">Enter the note hash</label>
+          <input type="text" name="url" id="url" className="form-control" />
+          <button type="submit" className="btn btn-primary">
+            Find Note
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Note;
